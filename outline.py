be11ce214fa9214b8bb4a12ebe42a87bff0f08fa
Patrick Olsen <patrick.d.olsen@icloud.com>
2022-08-02 13:19:38 -0400

    Changed from indeed to monster

--- outline.py
+++ outline.py
@@ -3,27 +3,22 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
+
 def displayJobDetails():
     print("Display job details")
 
 
-# function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
+# function to get job list from url 'https://www.monster.com/jobs/search?q={role}&where={location}'
 def getJobList(role, location):
-    url = 'https://www.indeed.com/jobs?q={role}&l={location}'
-    # Complete the missing part of this function here
-    #url = "https://www.indeed.com/jobs?q=Software Developer&l=Charlotte"
 
-    payload = {}
-    headers = {
-        'Cookie': 'CTK=1g9f9gc0ui7n4800; __cf_bm=rLC4cc.qWtc8NLgrGHHR7OgNqOiVxfhXfjm49jzYs1k-1659444408-0-AWH3gYigIG1/P0ndHehaDIRo01mlEdsJKc0SrnM2L/qoiB04DHhbDzRCgyx3GER1n6h3wlJCURbLYlP2usBrLaM=; _cfuvid=1yfTSHo3at37gTupJBr49bzOzFign.QuguxsdlqGECo-1659444408369-0-604800000; CMP_VISITED=1; INDEED_CSRF_TOKEN=S6ZC096E49XyRHxaxbZbPVekX3lXidQ3; JSESSIONID=BEFE39AD91F360EA0CF4157838A4BE21; LV="LA=1659444408:CV=1659444408:TS=1659444408"; PREF="TM=1659444408469:L=Charlotte"; RQ="q=Software+Developer&l=Charlotte&ts=1659444408496"; UD="LA=1659444408:CV=1659444408:TS=1659444408:SG=92dc7ca71441a235b5684f1b04a1deda"; indeed_rcc=CTK; jaSerpCount=1; mdserp=1'
-    }
-
-    response = requests.request("GET", url, headers=headers, data=payload)
+    url = 'https://www.monster.com/jobs/search?q={role}&where={location}'
+    
+    #response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.get(url)
 
     print(response.text)
 
     # save data in JSON file
-
 
 def saveDataInJSON(jobDetails):
     # Complete the missing part of this function here
@@ -39,8 +34,9 @@ def main():
     location = input()
 
     # Print verification to user
-    print("\nYour role is \"" + role + "\" and your location is \"" + location +"\".\n")
-    
+    print("\nYour role is \"" + role +
+          "\" and your location is \"" + location + "\".\n")
+
     # run getJobList
     getJobList(role, location)
 
